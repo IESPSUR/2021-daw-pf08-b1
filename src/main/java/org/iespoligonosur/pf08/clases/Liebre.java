9afc954be378348153d342b04b46c3ff9898d8cc
KimiaInfo <71088813+KimiaProg@users.noreply.github.com>
2021-01-10 13:58:03 +0100

    Update Liebre.java

--- src/main/java/org/iespoligonosur/pf08/clases/Liebre.java
+++ src/main/java/org/iespoligonosur/pf08/clases/Liebre.java
@@ -4,16 +4,16 @@ import java.util.Scanner;
 
 public class Liebre extends JugadorBasico {
 
-	//Variables
+	
+// Variables
 	TipoJugador tipo = TipoJugador.LIEBRE;
 	Dado dado = new Dado(6);
 
-	//El constructor
+	// El constructor
 	public Liebre(String nombre) {
 		super(nombre);
 	}
 
-	
 	public TipoJugador getTipo() {
 		return tipo;
 	}
@@ -22,38 +22,37 @@ public class Liebre extends JugadorBasico {
 	public void avanza() {
 		Scanner sc = new Scanner(System.in);
 		int dadoValor = dado.lanzarDado();
-		if (getRecorrido() >= 100) {
+
+		if (dadoValor == 3) {
+			setRecorridoTotal(dadoValor);
 			setUltimaTirada(dadoValor);
+			System.out.println(getCaminos() + getNombre());
 		} else {
-			if (dadoValor == 3) {
+			String mayMen;
+			if (dadoValor > 3) {
+				mayMen = "mayor";
+			} else {
+				mayMen = "menor";
+			}
+			boolean format = false;
+			String res;
+			do {
+				System.out.println(getNombre() + ", el número que ha salido es mayor o menor que 3? (mayor/menor)");
+				res = sc.next();
+				if (res.equalsIgnoreCase("menor") || res.equalsIgnoreCase("mayor")) {
+					format = true;
+				}
+			} while (format == false);
+			if (res.equalsIgnoreCase(mayMen)) {
+				setUltimaTirada(dadoValor);
 				setRecorridoTotal(dadoValor);
 				System.out.println(getCaminos() + getNombre());
 			} else {
-				String mayMen;
-				if (dadoValor > 3) {
-					mayMen = "mayor";
-				} else {
-					mayMen = "menor";
-				}
-				boolean format = false;
-				String res;
-				do {
-					System.out.println("El número que ha salido es mayor o menor que 3? (mayor/menor)");
-					res = sc.next();
-					if (res.equalsIgnoreCase("menor") || res.equalsIgnoreCase("mayor")) {
-						format = true;
-					}
-				} while (format == false);
-				if (res.equalsIgnoreCase(mayMen)) {
-					setRecorridoTotal(dadoValor);
-					System.out.println(dadoValor);
-					System.out.println(getCaminos() + getNombre());
-				}
+				System.out.println(getCaminos() + getNombre() + ": No puede mover");
 			}
+
 		}
 	}
 
-
-	
 
 }
