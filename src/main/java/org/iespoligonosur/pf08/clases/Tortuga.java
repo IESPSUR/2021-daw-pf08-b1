9ed2c2fe523850eab1fdfaaa98aaefd75e0c747d
Davidrj <davidroldan2901@gmail.com>
2021-01-29 20:58:16 +0100

    Tortuga david

--- src/main/java/org/iespoligonosur/pf08/clases/Tortuga.java
+++ src/main/java/org/iespoligonosur/pf08/clases/Tortuga.java
@@ -1,5 +1,33 @@
 package org.iespoligonosur.pf08.clases;
 
-public class Tortuga{
+
+public class Tortuga extends JugadorBasico {
+
+	// Variables
+	TipoJugador tipo = TipoJugador.TORTUGA;
+	Dado dado = new Dado(3);
+
+	// El constructor
+	public Tortuga(String nombre) {
+		super(nombre);
+	}
+    //El getter del tipo del jugador
+	public TipoJugador getTipo() {
+		return tipo;
+
+	}
+    /**
+     * Sobreescribir el método avanza de la interface IJugador
+     */
+	@Override
+	public void avanza() {
+		int dadoValor = dado.lanzarDado();
+		 
+			setRecorridoTotal(dadoValor);
+			setUltimaTirada(dadoValor);
+			System.out.println(getCaminos() + getNombre());
+		
+
+	}
 	
 }
