02ab3149cf6b0ff969d185caa9cce2937aeb7c62
MarinaAparicioSantos <marina.aparicio.santos.al@iespoligonosur.org>
2021-01-29 18:57:21 +0100

    correcaminos

--- src/main/java/org/iespoligonosur/pf08/clases/CorreCamino.java
+++ src/main/java/org/iespoligonosur/pf08/clases/CorreCamino.java
@@ -1,6 +1,38 @@
 package org.iespoligonosur.pf08.clases;
 
-public class CorreCamino {
+public class CorreCamino extends JugadorBasico {
 
+	//Las variables.
+	TipoJugador tipo = TipoJugador.CORRECAMINOS;
+	//Dado que devueve un numero entre 1 y 10.
+	Dado dado = new Dado(10);
 
+	//Constructor
+	public CorreCamino(String nombre) {
+		super(nombre);
+
+	}
+
+	//Get de TipoJugador
+	@Override
+	public TipoJugador getTipo() {
+
+		return null;
+	}
+
+	@Override
+	public void avanza() {
+		//Variable de tipo int al que se le ha asignado el metodo lanzarDado.
+		int dadoValor = dado.lanzarDado();
+		//Si el valor del dado es par avanza, si es impar no avanza.
+		if (dadoValor % 2 == 0) {
+			setRecorridoTotal(dadoValor);
+			setUltimaTirada(dadoValor);
+			System.out.println(getCaminos() + getNombre());
+
+		} else {
+			System.out.println(getCaminos() + getNombre() + " No puede avanzar.");
+		}
+
+	}
 }
